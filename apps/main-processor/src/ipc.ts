5ce80a6f479cbb69d139befe9de13dd489711235
Ashminita <ashminitab@mindfiresolutions.com>
2026-04-21 18:23:42 +0530

    refactor(ipc): add dark and default system theme to the electron window

--- apps/main-processor/src/ipc.ts
+++ apps/main-processor/src/ipc.ts
@@ -1,10 +1,24 @@
-import { ipcMain, dialog } from 'electron';
+import { ipcMain, dialog, nativeTheme } from 'electron';
 import { readFile } from './file';
 import { validateMarkdownFile, validatePath, validateSender } from './utils/ipc-validation';
 import { IPC_CONSTANTS } from '@package/shared-constants';
 
 //registers all IPC handlers for main process
 export function registerIPCHandlers(): void {
+  //application theme toggle
+  ipcMain.handle(IPC_CONSTANTS.TOGGLE_MODE, () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light';
+    } else {
+      nativeTheme.themeSource = 'dark';
+    }
+    return nativeTheme.shouldUseDarkColors;
+  });
+
+  //default system theme
+  ipcMain.handle(IPC_CONSTANTS.SYSTEM_MODE, () => {
+    nativeTheme.themeSource = 'system';
+  });
   //returns text content of the file
   ipcMain.handle(IPC_CONSTANTS.READ_FILE, async (event, filePath: string) => {
     if (!validateSender(event)) {
