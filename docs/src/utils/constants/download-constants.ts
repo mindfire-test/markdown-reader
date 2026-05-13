471c53de9b2c56960fab404a979aa8e78f73674e
Ashminita <ashminitab@mindfiresolutions.com>
2026-05-13 19:09:22 +0530

    feat(docs): add download links

---
+++ docs/src/utils/constants/download-constants.ts
@@ -0,0 +1,20 @@
+export const downloads = [
+  {
+    name: 'Windows',
+    format: '.exe installer',
+    href: '/downloads/Markdown-Reader-Setup-1.0.0.exe',
+    icon: '/img/downloads/windows.png',
+  },
+  {
+    name: 'macOS',
+    format: '.dmg package',
+    href: '/downloads/Markdown-Reader-1.0.0-arm64.dmg',
+    icon: '/img/downloads/macos.png',
+  },
+  {
+    name: 'Linux',
+    format: '.AppImage / .deb',
+    href: '/downloads/markdown-reader_1.0.0_amd64 .deb',
+    icon: '/img/downloads/linux.png',
+  },
+];
